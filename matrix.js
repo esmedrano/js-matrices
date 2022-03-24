5a28094293353d51af4cb9e7759dc6e67f329ca5
Elijah Seth Medrano <97810174+esmedrano@users.noreply.github.com>
2022-03-24 05:27:06 -0500

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,3 +1,17 @@
+function matmul(mat1, mat2) {  // multiply a given set of matrices
+ const new_mat = [];
+ var new_val = 0;
+ for (let i = 0; i < mat1.length; i++) {
+  for (let j = 0; j < mat2[0].length; j++) {
+   for (let a = 0; a < mat1[0].length; a++) {
+    new_val += mat1[i][a] * mat2[a][j];
+   }
+  }
+  new_mat.push(new_val);
+  new_val = 0;
+ }
+ return new_mat;
+}
 function transpose(mat) {  // transpose a given matrix
  const new_mat = [];  	
  for (let r = 0; r < mat[0].length; r++) {
@@ -5,20 +19,6 @@ function transpose(mat) {  // transpose a given matrix
   for (let c = 0; c < mat.length; c++) {
    new_mat[r].push(mat[c][r]);  // push the inverse of the current index
   } 
- }
- return new_mat;
-}
-function matmul(mat1, mat2) {  // multiply a given set of matrices
- const new_mat = [];
-	var new_val = 0;
- for (let i = 0; i < mat1.length; i++) {
-		for (let j = 0; j < mat2[0].length; j++) {
-   for (let a = 0; a < mat1[0].length; a++) {
-				new_val += mat1[i][a] * mat2[a][j];
- 		}
-	 }
-	 new_mat.push(new_val);
-	 new_val = 0;
  }
  return new_mat;
 }
