cc3fb5818eec7d6cf7ec639e4eb425ada18b7953
Elijah Seth Medrano <97810174+esmedrano@users.noreply.github.com>
2022-03-24 04:37:40 -0500

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,10 +1,10 @@
 function transpose(mat) {  // transpose a given matrix
-			const new_mat = [];  
-			for (let r = 0; r < mat[0].length; r++) {
-				new_mat.push([]);
-				for (let c = 0; c < mat.length; c++) {
-					new_mat[r].push(mat[c][r]);  // push the inverse of the current index
-				} 
-			}
-			return new_mat;
-		}
+ const new_mat = [];  	
+ for (let r = 0; r < mat[0].length; r++) {
+ new_mat.push([]);
+  for (let c = 0; c < mat.length; c++) {
+   new_mat[r].push(mat[c][r]);  // push the inverse of the current index
+  } 
+ }
+ return new_mat;
+}
