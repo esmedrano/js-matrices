9407bf80b2bea42ffd82ae616960afc2f500ea98
Elijah Seth Medrano <97810174+esmedrano@users.noreply.github.com>
2022-03-24 05:22:37 -0500

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -8,3 +8,17 @@ function transpose(mat) {  // transpose a given matrix
  }
  return new_mat;
 }
+function matmul(mat1, mat2) {  // multiply a given set of matrices
+ const new_mat = [];
+	var new_val = 0;
+ for (let i = 0; i < mat1.length; i++) {
+		for (let j = 0; j < mat2[0].length; j++) {
+   for (let a = 0; a < mat1[0].length; a++) {
+				new_val += mat1[i][a] * mat2[a][j];
+ 		}
+	 }
+	 new_mat.push(new_val);
+	 new_val = 0;
+ }
+ return new_mat;
+}
