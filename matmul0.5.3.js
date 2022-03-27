d16c6a5e9f830730e26cac0fe05fc8ea0a9e89f4
Elijah Seth Medrano <97810174+esmedrano@users.noreply.github.com>
2022-03-26 21:59:22 -0500

    Update and rename matmul0.5.2.js to matmul0.5.3.js

--- matmul0.5.3.js
+++ matmul0.5.3.js
@@ -2,7 +2,7 @@ function matmul(mat1, mat2) {  // multiply a given set of matrices
  const new_mat = [];
  var new_val = 0;
  for (let i = 0; i < mat1.length; i++) {
-  for (let j = 0; j < mat2.length; j++) {
+  for (let j = 0; j < mat2[0].length; j++) {
    for (let a = 0; a < mat1[0].length; a++) {
     new_val += mat1[i][a] * mat2[a][j];
    }
